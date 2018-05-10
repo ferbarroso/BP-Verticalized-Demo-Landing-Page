b93b6376be40f8c6c7425d8cd1ddf20061f6e823
ferbarroso <fbarroso@ufl.edu>
2018-05-10 00:00:52 -0400

    Initial Commit
    Adding All Files

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+    
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
